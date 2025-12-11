94a8869d4884f15a27388332c94866ff459780f1
DiegoJT22 <daigoestrlar22@gmail.com>
2025-12-11 12:39:42 -0500

    solución paquete 01 Ejercicio.java

--- EjerciciosT9/src/paquete01/Ejercicio.java
+++ EjerciciosT9/src/paquete01/Ejercicio.java
@@ -4,10 +4,51 @@
  */
 package paquete01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejercicio {
-    
-}
+    public static void main(String[] args) {
+         Scanner entrada = new Scanner(System.in);
+         
+         
+
+         String marca;
+         String mensajeFinal = "MARCAS\n";
+         int limite;
+         
+         
+        System.out.println("Ingrese limite de marcas");
+        limite = entrada.nextInt();
+        String[] marcas = new String[limite];
+
+         entrada.nextLine();
+        for (int i = 0; i < limite; i++) {
+            
+            System.out.println("Ingrese marca");
+            marca = entrada.nextLine();
+            marcas[i] = marca;
+            String letra = marca.substring(0, 1);
+            letra = letra.toUpperCase();
+            switch (letra) {
+                case "A":
+                case "C":
+                case "T":
+                    i = i - 1;
+                    break;
+                default:
+                    mensajeFinal += String.format("%s\n", marcas[i]);
+                    
+            }
+        }
+
+        System.out.printf("%s", mensajeFinal);
+
+         }
+        
+    }
+
+
