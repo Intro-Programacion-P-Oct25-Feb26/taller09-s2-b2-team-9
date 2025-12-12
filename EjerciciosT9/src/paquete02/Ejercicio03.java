090b71a04499b71c64a5726ef6d56a38c9e8c152
DiegoJT22 <daigoestrlar22@gmail.com>
2025-12-11 19:06:47 -0500

    solución paquete 02 Ejercicio03

--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -17,7 +17,30 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        
+        // para entender mejor donde esta la media {1, 2, 3, 4, 9, 10, 10, 11, 12, 12, 13, 16, 21}
+        double suma = 0;
+        int contador = 0;
+        String mensajeFinal = "";
+
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+            contador = contador + 1;
+
+        }
+        suma = suma / contador;
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] > suma) {
+
+                mensajeFinal += String.format("%d Arriba de la media\n", arreglo[i]);
+            } else {
+                mensajeFinal += String.format("%d Debajo de la media\n", arreglo[i]);
+            }
+
+        }
+        System.out.printf("Media aritmetica %.1f\n"
+                + "Lista de numeros\n"
+                + "%s", suma,mensajeFinal);
     }
 
 }
+
